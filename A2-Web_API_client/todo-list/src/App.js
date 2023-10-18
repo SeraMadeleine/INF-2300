b79df36e5cecc5a473f5f00cb546ad7a5cbd3449
Sera Madeleine Elstad <sel063@uit.no>
2023-10-18 17:49:51 +0200

    precode

---
+++ A2-Web_API_client/todo-list/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ToDoForm from './components/ToDoForm';
+
+function App() {
+  return (
+    <div className="Todo-list">
+     <ToDoForm />
+
+    </div>
+  );
+}
+
+export default App;
